b9d7987c0a2b516edb0cef87539f57f26f87de42
rcook0 <141237640+rcook0@users.noreply.github.com>
2025-09-27 12:54:27 +0200

    Update tb_soc.sv

--- rtl/tb_soc.sv
+++ rtl/tb_soc.sv
@@ -2,27 +2,23 @@
 #include "verilated.h"
 #include <iostream>
 
-
 vluint64_t main_time = 0;
-
 
 double sc_time_stamp() { return main_time; }
 
-
 int main(int argc, char **argv) {
-Verilated::commandArgs(argc, argv);
-Vsoc_top *top = new Vsoc_top;
+    Verilated::commandArgs(argc, argv);
+    Vsoc_top *top = new Vsoc_top;
 
-
-while (!Verilated::gotFinish() && main_time < 200000) {
-top->clk = (main_time & 1);
-top->eval();
-if (top->uart_tx_ready) {
-char c = (char)top->uart_tx_data;
-std::cout << c << std::flush;
-}
-main_time++;
-}
-delete top;
-return 0;
+    while (!Verilated::gotFinish() && main_time < 200000) {
+        top->clk = (main_time & 1);
+        top->eval();
+        if (top->uart_tx_ready) {
+            char c = (char)top->uart_tx_data;
+            std::cout << c << std::flush;
+        }
+        main_time++;
+    }
+    delete top;
+    return 0;
 }
