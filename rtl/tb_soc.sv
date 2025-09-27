7daa6d7e26084e63bf0fca18b7b97cae768b1f22
rcook0 <141237640+rcook0@users.noreply.github.com>
2025-09-27 12:52:57 +0200

    Create tb_soc.sv

---
+++ rtl/tb_soc.sv
@@ -0,0 +1,28 @@
+#include "Vsoc_top.h"
+#include "verilated.h"
+#include <iostream>
+
+
+vluint64_t main_time = 0;
+
+
+double sc_time_stamp() { return main_time; }
+
+
+int main(int argc, char **argv) {
+Verilated::commandArgs(argc, argv);
+Vsoc_top *top = new Vsoc_top;
+
+
+while (!Verilated::gotFinish() && main_time < 200000) {
+top->clk = (main_time & 1);
+top->eval();
+if (top->uart_tx_ready) {
+char c = (char)top->uart_tx_data;
+std::cout << c << std::flush;
+}
+main_time++;
+}
+delete top;
+return 0;
+}
