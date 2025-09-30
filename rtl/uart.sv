7c6ec96d998ff4889276bbec80b71d710904dfe4
rcook0 <141237640+rcook0@users.noreply.github.com>
2025-09-30 08:59:59 +0200

    Create uart.sv

---
+++ rtl/uart.sv
@@ -0,0 +1,15 @@
+module uart (
+    input logic clk,
+    input logic we,
+    input logic [7:0] din,
+    output logic [7:0] dout,
+    output logic tx_ready
+);
+    logic [7:0] tx_reg;
+    assign dout = tx_reg;
+    assign tx_ready = we;
+
+    always_ff @(posedge clk) begin
+        if (we) tx_reg <= din;
+    end
+endmodule
