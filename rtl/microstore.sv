efad298633510edbd724d0d33b775372eb50a4d7
rcook0 <141237640+rcook0@users.noreply.github.com>
2025-09-30 20:00:49 +0200

    Update microstore.sv

--- rtl/microstore.sv
+++ rtl/microstore.sv
@@ -1,12 +1,11 @@
+import z32u_pkg::*;
 module microstore (
-input logic clk,
-input logic [11:0] addr,
-output z32u_pkg::uinstr_t uinstr
+    input logic clk,
+    input logic [11:0] addr,
+    output uinstr_t uinstr
 );
-// Packed ROM: 96-bit microinstructions
-logic [95:0] rom [0:4095];
-initial $readmemh("microcode.hex", rom);
-
-
-assign uinstr = rom[addr];
+    // Packed ROM: 96-bit microinstructions
+    logic [95:0] rom [0:4095];
+    initial $readmemh("microcode.hex", rom);
+    assign uinstr = rom[addr];
 endmodule
